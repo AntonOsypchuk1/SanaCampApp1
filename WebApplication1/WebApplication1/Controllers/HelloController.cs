847636d0f245567f9150886de835368fd35b9c46
Anton Osypchuk <osypchuk.ua@gmail.com>
2023-03-23 19:19:07 +0200

    First lab commited

---
+++ WebApplication1/WebApplication1/Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloController : Controller
+    {
+        [HttpGet, Route("Home/Index"), Route("")]
+        public ActionResult Index(Name Con)
+        {
+            return View("~/Views/Home/Index.cshtml", Con);
+        }
+
+        [HttpPost]
+        public IActionResult Welcome(Name ConName)
+        {
+            return View("~/Views/Home/Welcome.cshtml", ConName);
+        }
+    }
+}
